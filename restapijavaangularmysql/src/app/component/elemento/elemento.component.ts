8db77d8d991735721497eee2a40ecfb0d856bddb
FrancoFelg <franco_lamberti_02@yahoo.com.ar>
2023-03-28 21:11:26 -0300

    Creación del Proyecto inicial
    Creación del proyecto y creación del componente Elemento

---
+++ restapijavaangularmysql/src/app/component/elemento/elemento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elemento',
+  templateUrl: './elemento.component.html',
+  styleUrls: ['./elemento.component.css']
+})
+export class ElementoComponent {
+
+}
